190d15ade9c66fc0a6aeabba899cf4cee75c4dc7
seo-yeongjun <zanygeek8371@gmail.com>
2023-10-22 23:00:19 +0900

    Dart HelloWorld

---
+++ .idea/StudyDart/HelloWorld.dart
@@ -0,0 +1,24 @@
+/*
+.dart 파일을 하나 만들고
+아래의 코드를 입력합니다.
+
+void main() {
+  print('Hello, World!');
+}
+
+터미널에 dart 파일명.dart 를 입력하면
+Hello, World! 가 출력됩니다.
+
+그렇다면 Hello [자신의이름]! 을 출력해보세요.
+*/
+
+
+void main(){
+ print("Hello 영준!");
+}
+
+// 배운 것 정리
+// 1. void main() 함수가 프로그램의 시작점이다.
+// 2. 문장 끝에는 ;을 붙인다.
+// 3. print() 함수로 출력한다.
+// 4. dart 파일을 실행하려면 dart 파일명.dart 를 입력한다.
